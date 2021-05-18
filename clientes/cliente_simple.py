862efac3ff73925b5c90732d09add8c3abf78fd7
Bruno Camargo <bruno7kp@outlook.com>
2021-05-18 19:38:30 -0300

    ask

--- clientes/cliente_simple.py
+++ clientes/cliente_simple.py
@@ -3,25 +3,17 @@ import json
 
 client = SoapClient(wsdl="http://localhost:8888")
 
-val1 = 10
-print('%s é ' % val1 + ('par' if client.verifica_NumeroPar(val1)['out0'] else 'impar'))
-
-val2 = 11
-print('%s é ' % val2 + ('par' if client.verifica_NumeroPar(val2)['out0'] else 'impar'))
-
-val3 = '529.982.247-25'
-print(val3 + ' é um CPF ' + ('válido' if client.verifica_CPF(val3)['out0'] else 'inválido'))
-
-val4 = '111.111.111-11'
-print(val4 + ' é um CPF ' + ('válido' if client.verifica_CPF(val4)['out0'] else 'inválido'))
-
-val5 = 10
-val6 = 5
-val7 = '*'
-print('{} {} {} = {}'.format(val5, val7, val6, client.verifica_Calculo(val5, val6, val7)['out0']))
+action = input('Digite "cpf" para validar um cpf, "calc" para realizar uma conta e qualquer outra coisa para sair:\n')
+while (action == "cpf" or action == "calc"):
+    if action == "cpf":
+        val3 = input('Digite um CPF para validar (com pontuação): ')
+        #val3 = '529.982.247-25'
+        print(val3 + ' é um CPF ' + ('válido' if client.verifica_CPF(val3)['out0'] else 'inválido'))
+    else:
+        val5 = input('Digite o primeiro número: ')
+        val6 = input('Digite o segundo número: ')
+        val7 = input('Digite o operador (+ - * / %): ')
+        print('{} {} {} = {}'.format(val5, val7, val6, client.verifica_Calculo(val5, val6, val7)['out0']))
+    action = input('Digite "cpf" para validar um cpf, "calc" para realizar uma conta e qualquer outra coisa para sair:\n')
 
 
-val8 = 10
-val9 = 5
-val10 = '/'
-print('{} {} {} = {}'.format(val8, val10, val9, client.verifica_Calculo(val8, val9, val10)['out0']))
